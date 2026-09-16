ca87a061f93dc90f4e2a7f94c79078129d4e3132
Юми Тян <apro91480@gmail.com>
2026-09-17 01:20:41 +0500

    Task3

---
+++ Tyuiu.YampolskyMO.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.YampolskyMO.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.YampolskyMO.Sprint0.Task3.V0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadLine();
+        }
+    }
+}
